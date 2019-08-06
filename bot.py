626ae28897657b239497fd6920f54aa8ed5bb4ba
chagitniss <chagitmail@gmail.com>
2019-08-06 05:22:29 +0300

    add help function, new game option and sent gifs function

--- bot.py
+++ bot.py
@@ -4,11 +4,11 @@ import model
 import settings
 import secret_settings
 
-from telegram.ext import Updater, CallbackQueryHandler
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, CallbackQueryHandler
+from telegram.ext import Updater
 from telegram.ext import MessageHandler, Filters
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-
+from telegram import ChatAction
 
 TOKEN = secret_settings.BOT_TOKEN
 updater = Updater(token=TOKEN)
@@ -21,6 +21,10 @@ logger = logging.getLogger(__name__)
 
 game = model.Game()
 
+def send_gif(bot, image_url, chat_id):
+    bot.sendChatAction(chat_id=chat_id, action=ChatAction.UPLOAD_PHOTO)
+    bot.sendDocument(chat_id=chat_id, document=image_url)
+
 
 def start(bot, update):
     chat_id = update.message.chat_id
@@ -28,20 +32,16 @@ def start(bot, update):
 
     game.add_user(chat_id)
     page_title = game.get_page_title(chat_id)
-    # settings.Expectation_button_click = True
 
     keyboard = [[InlineKeyboardButton("Yes", callback_data='2'), InlineKeyboardButton("No", callback_data='3')]]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    bot.send_message(chat_id=chat_id,
-                     text=f"Welcome To The Wiki Master Bot!\n")
+    bot.send_message(chat_id=chat_id, text=f"Welcome To The Wiki Master Bot!\n")
 
     bot.send_message(chat_id=chat_id,
                      text=f"Check out how much you know relative to wikipedia, the site that knows everything.\n")
 
-    bot.send_message(chat_id=chat_id,
-                     text=f"Let's start!\n"
-                          f"Have you heard of '{page_title}'?",
+    bot.send_message(chat_id=chat_id, text=f"Let's start!\nHave you heard of '{page_title}'?",
                      reply_markup=reply_markup)
 
 
@@ -51,18 +51,35 @@ def respond(bot, update):
     logger.info(f"= Got on chat #{chat_id}: {text!r}")
 
     message = game.test_word(text, chat_id)
-    bot.send_message(chat_id=chat_id, text=message)
+
+    if 'win' in message or 'fail' in message:
+        keyboard = [[InlineKeyboardButton("New Game", callback_data='1'),
+                    InlineKeyboardButton("Page Summary", callback_data='4')]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        send_gif(bot,message[message.index('url') + 3:], chat_id )
+        bot.send_message(chat_id=chat_id, text=message[:message.index('url')], reply_markup=reply_markup)
+
+    else:
+        bot.send_message(chat_id=chat_id, text=message)
 
 
 def button(bot, update):
     query = update.callback_query
     chat_id = query.message.chat_id
 
-    if query.data == '1':
+    if query.data == '1':  # new game
+        logger.info(f"= Got on chat #{chat_id}: pressed New Game button")
 
-        pass
+        game.add_user(chat_id)
+        page_title = game.get_page_title(chat_id)
 
-    elif query.data == '2':
+        keyboard = [[InlineKeyboardButton("Yes", callback_data='2'), InlineKeyboardButton("No", callback_data='3')]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        bot.send_message(chat_id=chat_id, text=f"Let's start!\n"
+                         f"Have you heard of '{page_title}'?", reply_markup=reply_markup)
+
+    elif query.data == '2':  # starting the guessing process
         logger.info(f"= Got on chat #{chat_id}: pressed Yes button")
         message = game.test_button_click('yes', chat_id)
 
@@ -70,7 +87,7 @@ def button(bot, update):
             bot.send_message(chat_id=chat_id, text="great!\nI just checked what Wikipedia knows about it.\n")
         bot.send_message(chat_id=chat_id, text=message)
 
-    elif query.data == '3':
+    elif query.data == '3':  # Another title is suggested
         logger.info(f"= Got on chat #{chat_id}: pressed No button")
         message = game.test_button_click('no', chat_id)
 
@@ -82,10 +99,18 @@ def button(bot, update):
             reply_markup = InlineKeyboardMarkup(keyboard)
             bot.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup)
 
-    # else:
-    #     keyboard = [[InlineKeyboardButton("Yes", callback_data='2'), InlineKeyboardButton("No", callback_data='3')]]
-    #     reply_markup = InlineKeyboardMarkup(keyboard)
-    #     bot.send_message(chat_id=chat_id, text=settings.INVALID_ANSWERS, reply_markup=reply_markup)
+    elif query.data == '4':
+        keyboard = [[InlineKeyboardButton("New Game", callback_data='1')]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        summary = game.get_info(chat_id)
+        bot.send_message(chat_id=chat_id, text=summary, reply_markup=reply_markup)
+
+
+def help(bot, update):
+    chat_id = update.message.chat_id
+    keyboard = [[InlineKeyboardButton("New Game", callback_data='1')]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    bot.send_message(chat_id=chat_id, text=settings.HELP_MESSAGE, reply_markup=reply_markup)
 
 
 start_handler = CommandHandler('start', start)
@@ -93,6 +118,9 @@ dispatcher.add_handler(start_handler)
 
 echo_handler = MessageHandler(Filters.text, respond)
 dispatcher.add_handler(echo_handler)
+
+help_handler = CommandHandler('help', help)
+dispatcher.add_handler(help_handler)
 
 updater.dispatcher.add_handler(CallbackQueryHandler(button))
 logger.info("Start polling")
